ce8c11d04432727159a8199577f9ad97708c109e
jhony2488 <jhon.araujo2488@gmail.com>
2021-09-14 20:49:15 -0300

    style:  :sparkles: setting up style guide

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@instagram': './src/instagram',
+        },
+      },
+    ],
+  ],
+  ignore: ['**/*.spec.ts'],
+}
